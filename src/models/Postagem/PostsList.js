78fbabc38b4eaab3156872f16267506c17d6cac6
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2024-04-17 15:18:24 -0300

    read

---
+++ src/models/Postagem/PostsList.js
@@ -0,0 +1,21 @@
+import Post from "./Post";
+
+class PostsList {
+    constructor(){
+        this.posts = [];
+    }
+
+    getAll(){
+        return this.posts;
+    }
+
+    getById(id){
+        return this.posts.find((post) => post.id === id);
+    }
+
+    addPost(post){
+        this.posts.push(post)
+    }
+
+    
+}
